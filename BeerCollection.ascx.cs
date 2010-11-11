92bf4111aa1089710736fb113c42e6bdd1ddb2ef
Ian Robinson <ian.andrew.robinson@gmail.com>
2010-11-11 11:48:40 -0600

    Added IActionable to BeerCollection for Add Beer.

--- BeerCollection.ascx.cs
+++ BeerCollection.ascx.cs
@@ -4,9 +4,26 @@
     using Components.Presenters;
     using DotNetNuke.Web.Mvp;
     using WebFormsMvp;
+    using DotNetNuke.Entities.Modules.Actions;
+    using DotNetNuke.Entities.Modules;
 
     [PresenterBinding(typeof(BeerCollectionPresenter))]
-    public partial class ViewBeerCollection : ModuleView<BeerCollectionModel>, IViewBeerCollection
+    public partial class ViewBeerCollection : ModuleView<BeerCollectionModel>, IViewBeerCollection, IActionable
     {
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                                  {
+                                      {
+                                          ModuleContext.GetNextActionID(), "Add Beer", ModuleActionType.AddContent, "",
+                                          "add.gif", ModuleContext.EditUrl(), false,
+                                          DotNetNuke.Security.SecurityAccessLevel.Edit, true, false
+                                          }
+                                  };
+                return actions;
+            }
+        }
     }
 }
