98e02784e56e20bde887346142cf6c6d25c443bd
ian.robinson <ian.andrew.robinson@gmail.com>
2011-03-25 11:13:18 -0500

    Updated DefaultTemplatePath to be dynamic. Added methods to get a template's contents.

--- Components/Templating/Template.cs
+++ Components/Templating/Template.cs
@@ -2,13 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Web;
     using Antlr3.ST;
     using DataRenderers;
 
     public static class Template
     {
         public static string DefaultGroupName = "Default";
-        public static string DefaultTemplatePath = @"c:\inetpub\wwwroot\dnnsource.dev\Website\DesktopModules\BeerCollection\Templates\Default";
+        public static string DefaultTemplatePath = HttpContext.Current.Server.MapPath("/DesktopModules/BeerCollection/Templates/Default");
 
         public static string RenderTemplate(string templateName, IEnumerable<KeyValuePair<string, object>> templateData)
         {
@@ -29,6 +30,17 @@
             template.RegisterRenderer(typeof(String), new BasicFormatRenderer());
 
             return template.ToString();
+        }
+
+        public static string GetTemplate(string templateName)
+        {
+            return GetTemplate(DefaultGroupName, DefaultTemplatePath, templateName);
+        }
+
+        public static string GetTemplate(string groupName, string groupPath, string templateName)
+        {
+            var templateGroup = new StringTemplateGroup(groupName, groupPath);
+            return templateGroup.GetInstanceOf(templateName).Template;
         }
     }
 }
